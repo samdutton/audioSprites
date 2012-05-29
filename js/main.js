7089672d7008f6cdfd142afb926ce8a20fac400b
Sam Dutton <samdutton@gmail.com>
2012-05-29 16:14:51 +0100

    added animal sounds

--- js/main.js
+++ js/main.js
@@ -2,12 +2,52 @@
 
 $(document).ready(function(){
 
-var audio = new Audio('audio/sprites.mp3');
-
+var audio = new Audio("audio/animalSounds.mp3");
+ 
 // if track element supported
 if (typeof audio.addTextTrack === "function") { 
   $(".isSupported").addClass("visible");
   $(".isNotSupported").addClass("hidden");
+  
+  
+  audio.addEventListener("loadedmetadata", function(){
+    var track = audio.addTextTrack("metadata", "sprite track", "en");    
+		track.mode = TextTrack.HIDDEN;
+
+    // for browsers that do not implement the getCueById() method
+    if (typeof track.getCueById !== "function") { 
+      track.getCueById = function(id){
+        var cues = track.cues;        
+        for (var i = 0; i != track.cues.length; ++i) {
+          if (cues[i].id === id) {
+            return cues[i];
+          }
+        }        
+      };
+    }
+
+    track.addCue(new TextTrackCue("purr", 0.000, 1.875, ""));
+    track.addCue(new TextTrackCue("kitten mew", 3.000, 4.000, ""));
+
+    var endTime;
+    audio.addEventListener("timeupdate", function (event) {
+      if (event.target.currentTime > endTime)
+        event.target.pause();
+    });
+        
+    function playSound(id) {
+        var cue = track.getCueById(id);
+        audio.currentTime = cue.startTime;
+        endTime = cue.endTime;
+        audio.play();
+    }
+    
+    $("button.playSound").click(function(){
+      playSound(this.id);
+    });
+       
+  });
+
 
 // if track element not supported
 } else {
@@ -16,18 +56,4 @@ if (typeof audio.addTextTrack === "function") {
 
 }
 
-// 
-//   var track = audio.addTextTrack('metadata');
-//     
-//   track.addCue(new TextTrackCue('dog bark', 12.783, 13.612, '', '', '', true));
-//   track.addCue(new TextTrackCue('kitten mew', 13.612, 15.091, '', '', '', true));
-//   
-//   function playSound(id) {
-//     audio.currentTime = track.getCueById(id).startTime;
-//     audio.play();
-//   }
-//   
-//   playSound('dog bark');
-//   playSound('kitten mew');
-} 
 });
